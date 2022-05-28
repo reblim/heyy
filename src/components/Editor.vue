75b1bda3833a7138b9f316a607d56200530b9cef
Milber Ferreira <milberferreira@hotmail.com>
2022-05-28 21:16:55 +0200

    Added heyy icons

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -43,6 +43,16 @@ export default {
 </script>
 
 <style lang="scss">
+@font-face {
+  font-family: 'heyy-icons';
+  src: url('../assets/fonts/heyy-icons.ttf') format('truetype'),
+    url('../assets/fonts/heyy-icons.woff') format('woff'),
+    url('../assets/fonts/heyy-icons.svg#heyy-icons') format('svg');
+  font-weight: normal;
+  font-style: normal;
+  font-display: block;
+}
+
 /*
 * Snow theme
 */
@@ -51,6 +61,32 @@ export default {
 /*
 * Heyy theme
 */
+
+@mixin heyyIcon {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'heyy-icons' !important;
+  font-size: 24px;
+  speak: never;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  color: #404040;
+  background-color: #dfdfdf;
+  width: 40px;
+  height: 40px;
+  border-radius: 2px;
+  margin: 0 1px;
+  border: 1px solid #bfbfbf;
+
+  /* Better Font Rendering =========== */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
 .ql-container {
   &.ql-snow {
     border: none;
@@ -77,6 +113,86 @@ export default {
 
   &.ql-snow {
     border: none;
+
+    .ql-formats {
+      margin-right: 0;
+    }
+  }
+}
+
+.ql-snow.ql-toolbar {
+  .ql-color {
+    @include heyyIcon;
+
+    .ql-picker-label {
+      @include heyyIcon;
+      border: none;
+      background-color: transparent;
+
+      svg {
+        display: none;
+      }
+
+      &::before {
+        content: '\e908';
+      }
+    }
+  }
+
+  .ql-background {
+    @include heyyIcon;
+
+    .ql-picker-label {
+      @include heyyIcon;
+      border: none;
+      background-color: transparent;
+
+      svg {
+        display: none;
+      }
+
+      &::before {
+        content: '\e904';
+      }
+    }
+  }
+}
+
+.ql-snow.ql-toolbar button {
+  @include heyyIcon;
+
+  svg {
+    display: none;
+  }
+
+  &.ql-bold {
+    &::before {
+      content: '\e905';
+    }
+  }
+
+  &.ql-italic {
+    &::before {
+      content: '\e912';
+    }
+  }
+
+  &.ql-underline {
+    &::before {
+      content: '\e91e';
+    }
+  }
+
+  &.ql-strike {
+    &::before {
+      content: '\e91b';
+    }
+  }
+
+  &.ql-strike {
+    &::before {
+      content: '\e91b';
+    }
   }
 }
 </style>
