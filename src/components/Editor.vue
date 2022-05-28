bc7f06db6a9c439646b17081b4a8d17af5ecc90f
Milber Ferreira <milberferreira@hotmail.com>
2022-05-28 08:21:24 +0200

    Initial clean up

---
+++ src/components/Editor.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="editor">
+    <h1>Editor</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editor-C',
+};
+</script>
+
+<style scoped lang="scss">
+.editor {
+}
+</style>
