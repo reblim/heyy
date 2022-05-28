ece793710301616151a6008b74cad5377bcf22db
Milber Ferreira <milberferreira@hotmail.com>
2022-05-28 08:39:56 +0200

    Added quill editor VI 🔥

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,16 +1,52 @@
 <template>
-  <div class="editor">
-    <h1>Editor</h1>
-  </div>
+  <div class="editor"></div>
 </template>
 
 <script>
+import Quill from 'quill';
+
 export default {
   name: 'Editor-C',
+
+  mounted() {
+    this.init();
+  },
+
+  methods: {
+    init() {
+      const options = {
+        // debug: 'info',
+        // readOnly: true,
+        // modules: {
+        //   toolbar: '#toolbar',
+        // },
+        placeholder: 'Compose an epic...',
+        theme: 'snow',
+      };
+
+      return new Quill(this.$el, options);
+    },
+  },
 };
 </script>
 
-<style scoped lang="scss">
-.editor {
+<style lang="scss">
+.ql-container {
+  &.ql-snow {
+    border: none;
+  }
+}
+
+.ql-editor {
+  position: relative;
+  max-width: 838px;
+  margin: 0 auto;
+  border: 2px solid red;
+
+  &.ql-blank {
+    &::before {
+      right: 16px;
+    }
+  }
 }
 </style>
