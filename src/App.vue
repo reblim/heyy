96a5bd9df15323fbb713f4933dc15386534d542f
Milber Ferreira <milberferreira@hotmail.com>
2022-05-29 11:38:50 +0200

    Added normalize

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
 <template>
-  <router-view />
+  <router-view class="app" />
 </template>
+
+<style lang="scss" scoped>
+@import 'normalize.css';
+
+html {
+}
+
+body {
+  box-sizing: border-box;
+
+  * {
+    box-sizing: inherit;
+  }
+}
+
+.app {
+  height: calc(100vh - 160px);
+}
+</style>
